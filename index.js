7126d71e34b8119c6cc53654c1b682ad7707f1bb
Emil Félix Muerrle <gitcommit@chaiya.es>
2019-11-09 15:32:19 +0100

    CP2

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-const svgWidth = '900';
-const svgHeight = '600';
-const chartWidth = '800';
-const chartHeight = '500';
+var margin = { top: 50, right: 50, bottom: 50, left: 50 };
+
+// Then define width and height as the inner dimensions of the chart area.
+
+var width = 960 - margin.left - margin.right;
+var height = 500 - margin.top - margin.bottom;
+
+// Lastly, define svg as a G element that translates the origin to the top-left corner of the chart area.
 
 var bodySvg = d3
   .select('body')
   .append('svg')
-  .attr('id', 'bodySvg')
-  .attr('width', svgWidth)
-  .attr('height', svgHeight);
+  .attr('width', width + margin.left + margin.right)
+  .attr('height', height + margin.top + margin.bottom)
+  .append('g')
+  .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-// var chartSvg = d3
-//   .select('svg')
+// var bodySvg = d3
+//   .select('body')
 //   .append('svg')
 //   .attr('id', 'bodySvg')
 //   .attr('width', svgWidth)
@@ -20,7 +25,7 @@ var bodySvg = d3
 bodySvg
   .append('text')
   .attr('id', 'title')
-  .attr('x', svgWidth / 2)
+  .attr('x', width / 2)
   .attr('y', 0 + 18)
   .attr('text-anchor', 'middle')
   .style('font-size', '14px')
@@ -31,17 +36,19 @@ d3.json(
 )
   .then(json => {
     const { data } = json;
-    const barWidth = Math.floor(svgWidth / data.length);
+    const barWidth = Math.floor(width / data.length);
 
     // console.log('data :', data);
+    console.log('width :', width);
+    console.log('height :', height);
     const scaleX = d3
       .scaleLinear()
       .domain([d3.min(data, d => d[0]), d3.max(data, d => d[0])])
-      .range([0, svgWidth]);
+      .range([0, width]);
     const scaleY = d3
       .scaleLinear()
       .domain([d3.min(data, d => d[1]), d3.max(data, d => d[1])])
-      .range([svgHeight, 0]);
+      .range([height, 0]);
 
     const axisX = d3.axisBottom().scale(scaleX);
     const axisY = d3.axisLeft().scale(scaleY);
@@ -63,10 +70,11 @@ d3.json(
       .append('rect')
       .attr('class', 'bar')
       .attr('width', barWidth - 1)
-      .attr('y', d => scaleY(d[1]))
+      .attr('x', d => margin.left)
+      .attr('y', d => scaleY(d[1]) + margin.top)
       .attr('height', d => {
         // console.log('d[1] :', d[1]);
-        return svgHeight - scaleY(d[1]);
+        return height - scaleY(d[1]);
       })
       .attr('transform', (d, i) => {
         return 'translate(' + i * barWidth + ')';
