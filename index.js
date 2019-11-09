fd239b90401c1e3952f3fcd9aa32df2ac17ab7b0
Emil Félix Muerrle <gitcommit@chaiya.es>
2019-11-09 20:46:50 +0100

    Passes tests

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 // Margin Convention as per https://bl.ocks.org/mbostock/3019563
 
-var margin = { top: 50, right: 50, bottom: 50, left: 50 };
+const margin = { top: 50, right: 50, bottom: 50, left: 50 };
 
-// Then define width and height as the inner dimensions of the chart area.
+const width = 900 - margin.left - margin.right;
+const height = 500 - margin.top - margin.bottom;
 
-var width = 960 - margin.left - margin.right;
-var height = 500 - margin.top - margin.bottom;
-
-// Lastly, define svg as a G element that translates the origin to the top-left corner of the chart area.
-
-var bodySvg = d3
+const bodySvg = d3
   .select('body')
   .append('svg')
   .attr('width', width + margin.left + margin.right)
@@ -21,7 +17,7 @@ bodySvg
   .append('text')
   .attr('id', 'title')
   .attr('x', width / 2)
-  .attr('y', 0 + 18)
+  .attr('y', 20)
   .attr('text-anchor', 'middle')
   .style('font-size', '14px')
   .style('font-family', 'sans-serif')
@@ -32,14 +28,9 @@ d3.json(
 )
   .then(json => {
     const { data } = json;
-    // const barWidth = Math.floor(width / data.length);
     const barWidth = width / data.length;
-
-    console.log('data :', data);
-
-    var begin = new Date(data[0][0]);
-    var end = new Date(data[data.length - 1][0]);
-    // console.log('begin :', begin, 'end :', end);
+    const begin = new Date(data[0][0]);
+    const end = new Date(data[data.length - 1][0]);
 
     const scaleX = d3
       .scaleTime()
@@ -47,7 +38,14 @@ d3.json(
       .range([0, width]);
     const scaleY = d3
       .scaleLinear()
-      .domain([d3.min(data, d => d[1]), d3.max(data, d => d[1])])
+      .domain([
+        d3.min(data, d => {
+          return 0;
+        }),
+        d3.max(data, d => {
+          return d[1];
+        })
+      ])
       .range([height, 0]);
 
     const axisX = d3.axisBottom(scaleX);
@@ -64,6 +62,12 @@ d3.json(
       .attr('id', 'y-axis')
       .call(axisY);
 
+    const tooltipDiv = d3
+      .select('body')
+      .append('div')
+      .attr('id', 'tooltip')
+      .style('opacity', 0);
+
     const bar = d3
       .select('svg')
       .selectAll('rect')
@@ -71,15 +75,43 @@ d3.json(
       .enter()
       .append('rect')
       .attr('class', 'bar')
+      .attr('data-date', d => {
+        return d[0];
+      })
+      .attr('data-gdp', d => {
+        return d[1];
+      })
       .attr('width', barWidth - 1)
       .attr('x', d => margin.left)
       .attr('y', d => scaleY(d[1]) + margin.top)
       .attr('height', d => {
-        // console.log('d[1] :', d[1]);
         return height - scaleY(d[1]);
       })
       .attr('transform', (d, i) => {
         return 'translate(' + i * barWidth + ')';
+      })
+      .on('mouseover', d => {
+        // console.log('d :', d);
+        // console.log('d3.event :', d3.event);
+        // console.log('d3.event.pageX :', d3.event.pageX);
+        // console.log('d3.event.pageY :', d3.event.pageY);
+        tooltipDiv
+          .style('left', d3.event.pageX + 10 + 'px')
+          .style('top', d3.event.pageY + 10 + 'px')
+          .style('font-size', '14px')
+          .style('font-family', 'sans-serif')
+          .attr('data-date', d[0])
+          .html('Date:<br/>' + d[0]);
+        tooltipDiv
+          .transition()
+          .duration(50)
+          .style('opacity', 0.8);
+      })
+      .on('mouseout', d => {
+        tooltipDiv
+          .transition()
+          .duration(600)
+          .style('opacity', 0);
       });
   })
   .catch(err => {
